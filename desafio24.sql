6ba5f4db4846069c846e191b099b264d3148b5f7
Felipe Matheus <fmatheusog@gmail.com>
2022-03-02 19:41:50 -0300

    projeto concluido

--- desafio24.sql
+++ desafio24.sql
@@ -1,1 +1,1 @@
-UPDATE order_details SET discount = 45 WHERE unit_price > 10.0000 AND (id > 30 or id < 40)
+UPDATE order_details SET discount = 45 WHERE unit_price > 10.0000 AND (id >= 30 AND id <= 40)
