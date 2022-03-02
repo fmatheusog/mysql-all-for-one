5880b9147dd989f7c101b3e6128fe1884c7fb9a6
Felipe Matheus <fmatheusog@gmail.com>
2022-03-02 19:24:07 -0300

    fix: wrong number req 24

--- desafio24.sql
+++ desafio24.sql
@@ -1,1 +1,1 @@
-UPDATE order_details SET discount = 45 WHERE unit_price > 10.0000 AND (id > 10 or id < 40)
+UPDATE order_details SET discount = 45 WHERE unit_price > 10.0000 AND (id > 30 or id < 40)
